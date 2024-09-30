983c17d12836d8fd69a073a47cb8877282cb2832
abel masingita <abelmasingita9@gmail.com>
2024-09-30 23:22:19 +0200

    RandomWordSolver complete

--- src/components/RandomWordSolver.tsx
+++ src/components/RandomWordSolver.tsx
@@ -1,57 +1,87 @@
 import {
-  Alert,
   Box,
   Button,
   CircularProgress,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
   TextField,
   Typography,
 } from '@mui/material'
 import { useState } from 'react'
+import { SolvePuzzle } from '../util/interfaces/SolvePuzzle'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
-function solveRandomizedWordSequence(sequence: string) {
+const SolveRandomizedWordSequence = (sequence: string) => {
   const numberWords = [
-    'zero',
-    'one',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine',
-    'ten',
+    { word: 'zero', uniqueChar: 'z', value: 0 },
+    { word: 'two', uniqueChar: 'w', value: 2 },
+    { word: 'four', uniqueChar: 'u', value: 4 },
+    { word: 'six', uniqueChar: 'x', value: 6 },
+    { word: 'eight', uniqueChar: 'g', value: 8 },
+    { word: 'one', uniqueChar: 'o', value: 1 },
+    { word: 'three', uniqueChar: 'h', value: 3 },
+    { word: 'five', uniqueChar: 'f', value: 5 },
+    { word: 'seven', uniqueChar: 'v', value: 7 },
+    { word: 'nine', uniqueChar: 'i', value: 9 },
+    { word: 'ten', uniqueChar: 't', value: 10 },
   ]
 
-  const result = []
-
-  for (let i = 0; i < numberWords.length; i++) {
-    const word = numberWords[i]
-    const count = (sequence.match(new RegExp(word, 'g')) || []).length
-
-    if (count > 0) {
-      result.push({ value: i, word, count })
-    }
+  //requency map for all characters in the sequence
+  const charCount: { [key: string]: number } = {}
+  for (const char of sequence) {
+    charCount[char] = (charCount[char] || 0) + 1
   }
 
-  console.log('in :: ', result)
+  const result: SolvePuzzle[] = []
 
-  const formattedResult = result
-    .map((item) => `${item.value}: ${item.word} (${item.count})`)
-    .join('\n')
+  //Iterate  and count occurrences
+  numberWords.forEach(({ word, uniqueChar, value }) => {
+    // If the unique character exists in the sequence, count how many times
+    const count = charCount[uniqueChar] || 0
+    if (count > 0) {
+      result.push({ value, word, count })
 
-  return formattedResult
+      //Reduce the frequency of characters forming this word
+      for (const char of word) {
+        charCount[char] -= count
+      }
+    }
+  })
+
+  return result
 }
 
 const RandomWordSolver: React.FC = () => {
   const [wordSequence, setWordSequence] = useState('')
-  const [item, setItem] = useState('')
+  const [items, setItems] = useState<SolvePuzzle[]>([])
   const [loading, setLoading] = useState(false)
-  const [error] = useState('')
+  const MySwal = withReactContent(Swal)
 
-  const handlSolve = () => {
+  const handleSolve = async () => {
     setLoading(true)
-    setItem(solveRandomizedWordSequence(wordSequence))
+
+    if (wordSequence.trim().length > 0) {
+      const results = await new Promise<SolvePuzzle[]>((resolve) => {
+        setTimeout(() => {
+          resolve(SolveRandomizedWordSequence(wordSequence))
+        }, 1000)
+      })
+
+      setItems(results)
+    } else {
+      MySwal.fire({
+        title: 'Oops!',
+        text: 'Input must not be empty',
+        icon: 'warning',
+      })
+    }
+
     setLoading(false)
   }
 
@@ -69,7 +99,6 @@ const RandomWordSolver: React.FC = () => {
           <Typography variant='body1'>Processing...</Typography>
         </Box>
       )}
-      {error && <Alert severity='error'>{error}</Alert>}
 
       <TextField
         label='Word Sequence'
@@ -80,17 +109,39 @@ const RandomWordSolver: React.FC = () => {
         onChange={(e) => setWordSequence(e.target.value)}
       />
       <Button
-        onClick={handlSolve}
+        onClick={handleSolve}
         variant='contained'
         sx={{ mb: 2, width: '50%' }}
       >
         Solve
       </Button>
 
-      {item && (
-        <Typography variant='subtitle1' sx={{ m: 2 }}>
-          Result: {item || ''}
-        </Typography>
+      {items.length > 0 && (
+        <Box>
+          <TableContainer component={Paper} sx={{ flex: 1 }}>
+            <Table sx={{ minWidth: 700 }} aria-label='simple table'>
+              <TableHead>
+                <TableRow>
+                  <TableCell align='right'>Value</TableCell>
+                  <TableCell align='right'>Word</TableCell>
+                  <TableCell align='right'>Word Count</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {items.map((row) => (
+                  <TableRow
+                    key={row.value}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell align='right'>{row.value}</TableCell>
+                    <TableCell align='right'>{row.word}</TableCell>
+                    <TableCell align='right'>{row.count}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
       )}
     </Box>
   )
